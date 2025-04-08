a899b753a87c6aaeb74fee4869a5384d9f8b7644
minwoo <minals0520@naver.com>
2025-04-08 16:06:22 +0900

    Fail : Math | [BOJ] 1959 달팽이3 | 문제풀이중

--- minwoo/snail.py
+++ minwoo/snail.py
@@ -1,29 +1,25 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-R, C = map(int, input().split())
+R, C = map(int, sys.stdin.readline().split())
 visited_row, visited_col = set(), set()
 row, col = 0, C-1
 rc, cc = 0, 0
-# stop_row, stop_col =
+
 i = 0
+prev_row, prev_col = row, col
 while True:
     i += 1
     # 짝수는 열을 변경
     if i % 2 == 0:
         col = C-1 - (col - cc)
-        if col in visited_col:
-            break
-        visited_col.add(col)
         cc = -(1 if cc == 0 else 0)
 
     # 홀수는 행을 변경
     else:
         row = R-1 - (row + rc)
-        if row in visited_row:
-            break
-        visited_row.add(row)
         rc = -(1 if rc == 0 else 0)
-
-
+    if prev_row == row and prev_col == col:
+        break
+    prev_row, prev_col = row, col
 print(f'{i-1}\n{row+1} {col+1}')
