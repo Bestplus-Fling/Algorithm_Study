078444c2494c5cf15c009deef60aadf96e0a70ec
bestplus <minals0520@naver.com>
2025-04-22 00:18:06 +0900

    [Solve] : IM Prepare

--- minwoo/Brute_Force/SWEA_22399_250411/sol2.py
+++ minwoo/Brute_Force/SWEA_22399_250411/sol2.py
@@ -5,13 +5,13 @@ T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     carrot = list(map(int, input().split()))
-    carrot_box = [0] * 31
+    carrot = [0] * 31
     for i in carrot:
-        carrot_box[i] += 1
+        carrot[i] += 1
     result = float('inf')
     for i in range(31):
         for j in range(i, 31):
-            small, middle, big = sum(carrot_box[:i]), sum(carrot_box[i:j]), sum(carrot_box[j:])
+            small, middle, big = sum(carrot[:i]), sum(carrot[i:j]), sum(carrot[j:])
             temp = [small, middle, big]
             if 0 in temp:
                 continue
